edbc63f99b3d9a06dba6885bd6f54508bf339064
rey-coyrehourcq <sebastien.rey-coyrehourcq@univ-rouen.fr>
2020-10-20 14:22:13 +0200

    fix shell script

--- get-pikchr.sh
+++ get-pikchr.sh
@@ -4,6 +4,8 @@ wget https://pikchr.org/home/tarball/b1d01d99f9/pikchr-b1d01d99f9.tar.gz
 [[ -d pikchr-latest ]] && rm -r pikchr-latest
 
 mkdir pikchr-latest
-tar -zxvf pikchr-b1d01d99f9.tar.gz -C pikchr-latest
-
+tar -xvzf pikchr-b1d01d99f9.tar.gz
+mv ./pikchr-b1d01d99f9/* ./pikchr-latest
+rm -r ./pikchr-b1d01d99f9/
+rm pikchr-b1d01d99f9.tar.gz
 
